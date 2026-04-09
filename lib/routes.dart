3549d9bdc2db05ddf642ece17233ffa8ad312e85
Flávio Sebastião <jossebastiao472@gmail.com>
2026-04-09 09:35:51 +0100

    novaversao

--- lib/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'screens/home/home_screen.dart';
-import 'screens/group/create_group_screen.dart';
-
-final Map<String, WidgetBuilder> appRoutes = {
-  '/': (context) => HomeScreen(),
-  '/create': (context) => const CreateGroupScreen(),
-};
